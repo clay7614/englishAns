7cb51b7b8af79cabc5942816ad83c5c74a62207e
clay <167842615+clay7614@users.noreply.github.com>
2025-12-19 11:03:56 +0900

    update: ヘッダーの履歴設定ボタンを大きく調整

--- app.js
+++ app.js
@@ -1105,8 +1105,10 @@ function handleDuplicatesToggleChange() {
 function updateDarkMode() {
   if (darkModeEnabled) {
     document.documentElement.classList.add('dark-mode');
+    document.documentElement.classList.remove('light-mode');
   } else {
     document.documentElement.classList.remove('dark-mode');
+    document.documentElement.classList.add('light-mode');
   }
   if (darkModeCheckbox) {
     darkModeCheckbox.checked = darkModeEnabled;
